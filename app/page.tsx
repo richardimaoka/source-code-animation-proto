dc12982ba040d9451af1b1c50cdd70a9febba0dd
richardimaoka <richard.s.imaoka@gmail.com>
2025-09-13 16:49:52 +0900

    RSC shiki

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <div>hello world</div>;
+import type { BundledLanguage } from "shiki";
+import { codeToHtml } from "shiki";
+
+export default function Page() {
+  return (
+    <main>
+      <CodeBlock lang="ts">
+        {['console.log("Hello")', 'console.log("World")'].join("\n")}
+      </CodeBlock>
+    </main>
+  );
+}
+
+interface Props {
+  children: string;
+  lang: BundledLanguage;
+}
+
+async function CodeBlock(props: Props) {
+  const out = await codeToHtml(props.children, {
+    lang: props.lang,
+    theme: "github-dark",
+  });
+
+  return <div dangerouslySetInnerHTML={{ __html: out }} />;
 }
